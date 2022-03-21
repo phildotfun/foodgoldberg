75c9b8e04669a82e4e9e172ffbc0465dc01313e8
Phil Robibero <pasr.contact@gmail.com>
2022-03-21 11:02:59 -0400

    Started creating a contoller

---
+++ Foodgoldberg/Assets/Scripts/Controller.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+   GameObject
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+        }
+    }
+
+    void ObjectActive(bool)
+    {
+
+    }
+}
+
+
